129f87689d1ece5781310cf4222569c556c56b5e
Craig Miller <craigmiller160@gmail.com>
2023-02-24 13:36:13 -0500

    Got koin working

--- src/main/kotlin/io/craigmiller160/plugins/Koin.kt
+++ src/main/kotlin/io/craigmiller160/plugins/Koin.kt
@@ -2,8 +2,16 @@ package io.craigmiller160.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.application.Application
+import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
+class Hello {
+  fun sayHello(): String = "Hello World"
+}
+
 fun Application.configureKoin() {
-  install(Koin) {}
+  install(Koin) {
+    val module = module { single { Hello() } }
+    modules(module)
+  }
 }
