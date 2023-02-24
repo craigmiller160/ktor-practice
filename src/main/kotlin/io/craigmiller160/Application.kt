2303b4e7438fd2aa6c0e41daf4d84a336da4fde8
Craig Miller <craigmiller160@gmail.com>
2023-02-23 20:32:00 -0500

    Setting up app

---
+++ src/main/kotlin/io/craigmiller160/Application.kt
@@ -0,0 +1,17 @@
+package io.craigmiller160
+
+import io.craigmiller160.plugins.configureRouting
+import io.craigmiller160.plugins.configureSecurity
+import io.craigmiller160.plugins.configureSerialization
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.EngineMain
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
