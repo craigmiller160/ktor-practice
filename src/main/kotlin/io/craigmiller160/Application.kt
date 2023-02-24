9bcae9ff1079d9df3954431e1b319bd5b65af237
Craig Miller <craigmiller160@gmail.com>
2023-02-23 21:03:11 -0500

    Setup routes

--- src/main/kotlin/io/craigmiller160/Application.kt
+++ src/main/kotlin/io/craigmiller160/Application.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160
 
+import io.craigmiller160.people.peopleRoutes
 import io.craigmiller160.plugins.configureRouting
 import io.craigmiller160.plugins.configureSecurity
 import io.craigmiller160.plugins.configureSerialization
@@ -7,11 +8,16 @@ import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.netty.Netty
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.routing
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
 //    configureSecurity()
     configureSerialization()
-    configureRouting()
+//    configureRouting()
+    routing {
+        peopleRoutes()
+    }
 }
--- src/main/kotlin/io/craigmiller160/people/Routes.kt
+++ src/main/kotlin/io/craigmiller160/people/Routes.kt
@@ -3,9 +3,18 @@ package io.craigmiller160.people
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 
 private val people = mutableListOf<Person>(Person("Bob", 30))
+
+fun Routing.peopleRoutes() {
+//    createPerson()
+//    updatePerson()
+//    getPerson()
+//    deletePerson()
+    getAllPeople()
+}
 
 fun Route.createPerson() {
 
