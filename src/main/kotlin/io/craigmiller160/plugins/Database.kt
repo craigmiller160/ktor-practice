ba2896382a73b33c000e7fd43f214e86b307ffcb
Craig Miller <craigmiller160@gmail.com>
2023-02-24 13:42:44 -0500

    Progress with setup

--- src/main/kotlin/io/craigmiller160/plugins/Database.kt
+++ src/main/kotlin/io/craigmiller160/plugins/Database.kt
@@ -3,10 +3,14 @@ package io.craigmiller160.plugins
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
+import javax.sql.DataSource
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
+import org.koin.dsl.module
+import org.koin.ktor.ext.inject
+import org.koin.ktor.plugin.koin
 
-fun Application.configureDatabase() {
+fun Application.createDatabaseDependencies() {
   val config =
       HikariConfig().apply {
         jdbcUrl = environment.config.property("postgres.jdbcUrl").getString()
@@ -14,7 +18,15 @@ fun Application.configureDatabase() {
         password = environment.config.property("postgres.password").getString()
       }
 
-  val datasource = HikariDataSource(config)
+  koin {
+    val module = module { single<DataSource> { HikariDataSource(config) } }
+    modules(module)
+  }
+}
+
+fun Application.configureDatabase() {
+  createDatabaseDependencies()
+  val datasource by inject<DataSource>()
   Database.connect(datasource)
 
   Flyway.configure().dataSource(datasource).load().migrate()
