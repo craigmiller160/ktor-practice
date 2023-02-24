402044b0259d604244cce7330b6dd135e2febd94
Craig Miller <craigmiller160@gmail.com>
2023-02-23 21:10:18 -0500

    Finished people routes

--- src/main/kotlin/io/craigmiller160/people/Person.kt
+++ src/main/kotlin/io/craigmiller160/people/Person.kt
@@ -1,5 +1,8 @@
 package io.craigmiller160.people
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Person(
     val name: String,
     val age: Int
--- src/main/kotlin/io/craigmiller160/people/Routes.kt
+++ src/main/kotlin/io/craigmiller160/people/Routes.kt
@@ -1,35 +1,56 @@
 package io.craigmiller160.people
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.Routing
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
 
 private val people = mutableListOf<Person>(Person("Bob", 30))
 
 fun Routing.peopleRoutes() {
-//    createPerson()
-//    updatePerson()
-//    getPerson()
-//    deletePerson()
+    createPerson()
+    updatePerson()
+    getPerson()
+    deletePerson()
     getAllPeople()
 }
 
 fun Route.createPerson() {
-
+    post("/people") {
+        val person = call.receive<Person>()
+        people += person
+        call.response.status(HttpStatusCode.Created)
+    }
 }
 
 fun Route.updatePerson() {
-
+    put("/people/{index}") {
+        val person = call.receive<Person>()
+        val index = call.parameters["index"]?.toInt()
+        people[index!!] = person
+        call.response.status(HttpStatusCode.NoContent)
+    }
 }
 
 fun Route.getPerson() {
-
+    get("/people/{index}") {
+        val index = call.parameters["index"]?.toInt()
+        call.respond(people[index!!])
+    }
 }
 
 fun Route.deletePerson() {
-
+    delete("/people/{index}") {
+        val index = call.parameters["index"]?.toInt()
+        people.removeAt(index!!)
+        call.response.status(HttpStatusCode.NoContent)
+    }
 }
 
 fun Route.getAllPeople() {
