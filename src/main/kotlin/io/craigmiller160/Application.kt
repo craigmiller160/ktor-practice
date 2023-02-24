4e5db1b50e0a4c9f5790937e6f8ff6699eb0ea90
Craig Miller <craigmiller160@gmail.com>
2023-02-24 13:53:27 -0500

    More refactoring

--- src/main/kotlin/io/craigmiller160/Application.kt
+++ src/main/kotlin/io/craigmiller160/Application.kt
@@ -1,24 +1,13 @@
 package io.craigmiller160
 
-import io.craigmiller160.people.peopleRoutes
-import io.craigmiller160.plugins.configureDatabase
 import io.craigmiller160.plugins.configureKoin
 import io.craigmiller160.plugins.configureSerialization
-import io.craigmiller160.plugins.createDatabaseDependencies
 import io.ktor.server.application.Application
 import io.ktor.server.netty.EngineMain
-import io.ktor.server.routing.routing
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-fun Application.module() {
+fun Application.rootModule() {
   configureKoin()
-  configureDatabase()
   configureSerialization()
-  createDatabaseDependencies()
-  routing { peopleRoutes() }
-}
-
-fun Application.createDatabaseDependencies() {
-  println("THIS IS WORKING!!!")
 }
