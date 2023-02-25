5bf6046c733fe5d8fad47cea5970ca10d23d9e8d
Craig Miller <craigmiller160@gmail.com>
2023-02-25 12:52:34 -0500

    Added tc extension

--- src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
@@ -1,11 +1,14 @@
 package io.craigmiller160
 
+import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(TestcontainersExtension::class)
 class PeopleRoutesTest {
   @Test
   fun testRoot() = testApplication {
