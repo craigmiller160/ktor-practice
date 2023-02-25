66a33c4aa88589740c2fde10ad374a097646bd2a
Craig Miller <craigmiller160@gmail.com>
2023-02-25 15:41:10 -0500

    Refactored to use services for everything

--- src/main/kotlin/io/craigmiller160/people/Routes.kt
+++ src/main/kotlin/io/craigmiller160/people/Routes.kt
@@ -1,19 +1,11 @@
 package io.craigmiller160.people
 
-import io.craigmiller160.database.appTransaction
-import io.craigmiller160.people.domain.entity.Person
-import io.craigmiller160.people.domain.entity.newWithRequest
-import io.craigmiller160.people.domain.entity.toResponse
-import io.craigmiller160.people.domain.table.People
-import io.craigmiller160.people.domain.table.updateWithRequest
 import io.craigmiller160.people.dto.PersonRequest
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.application
 import io.ktor.server.application.call
-import io.ktor.server.application.log
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
-import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.delete
@@ -21,8 +13,6 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import java.util.UUID
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.deleteWhere
 
 fun Routing.peopleRoutes() {
   createPerson()
@@ -34,38 +24,37 @@ fun Routing.peopleRoutes() {
 
 fun Route.createPerson() {
   post("/people") {
-    application.log.info("Create Person")
     val person = call.receive<PersonRequest>()
-    val dbPerson = appTransaction { Person.newWithRequest(person) }
+    val response = application.peopleService().createPerson(person)
     call.response.status(HttpStatusCode.Created)
-    call.respondText(dbPerson.id.value.toString())
+    call.respond(response)
   }
 }
 
 fun Route.updatePerson() {
   put("/people/{id}") {
-    application.log.info("Update Person")
     val person = call.receive<PersonRequest>()
     val id = UUID.fromString(call.parameters["id"])
-    appTransaction { People.updateWithRequest(id, person) }
-    call.response.status(HttpStatusCode.NoContent)
+    if (application.peopleService().updatePerson(id, person)) {
+      call.response.status(HttpStatusCode.NoContent)
+    } else {
+      call.response.status(HttpStatusCode.NotFound)
+    }
   }
 }
 
 fun Route.getPerson() {
   get("/people/{id}") {
-    application.log.info("Get Person")
     val id = UUID.fromString(call.parameters["id"])
-    appTransaction { Person.findById(id) }?.let { call.respond(it.toResponse()) }
-        ?: call.response.status(HttpStatusCode.NoContent)
+    application.peopleService().getPerson(id)?.let { call.respond(it) }
+        ?: call.response.status(HttpStatusCode.NotFound)
   }
 }
 
 fun Route.deletePerson() {
   delete("/people/{id}") {
-    application.log.info("Delete Person")
     val id = UUID.fromString(call.parameters["id"])
-    appTransaction { People.deleteWhere { People.id eq id } }
+    application.peopleService().deletePerson(id)
     call.response.status(HttpStatusCode.NoContent)
   }
 }
--- src/main/kotlin/io/craigmiller160/people/Services.kt
+++ src/main/kotlin/io/craigmiller160/people/Services.kt
@@ -2,10 +2,18 @@ package io.craigmiller160.people
 
 import io.craigmiller160.database.appTransaction
 import io.craigmiller160.people.domain.entity.Person
+import io.craigmiller160.people.domain.entity.newWithRequest
 import io.craigmiller160.people.domain.entity.toResponse
+import io.craigmiller160.people.domain.table.People
+import io.craigmiller160.people.domain.table.updateWithRequest
+import io.craigmiller160.people.dto.PersonRequest
 import io.craigmiller160.people.dto.PersonResponse
 import io.ktor.server.application.Application
+import io.ktor.server.application.application
 import io.ktor.server.application.log
+import java.util.UUID
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
 
 class PeopleService(val application: Application)
 
@@ -15,4 +23,24 @@ suspend fun PeopleService.getAllPeople(): List<PersonResponse> {
   application.log.info("Get All People")
   val list = appTransaction { Person.all().toList() }
   return list.map { it.toResponse() }
+}
+
+suspend fun PeopleService.deletePerson(id: UUID) {
+  application.log.info("Delete Person")
+  appTransaction { People.deleteWhere { People.id eq id } }
+}
+
+suspend fun PeopleService.getPerson(id: UUID): PersonResponse? {
+  application.log.info("Get Person")
+  return appTransaction { Person.findById(id) }?.toResponse()
+}
+
+suspend fun PeopleService.updatePerson(id: UUID, request: PersonRequest): Boolean {
+  application.log.info("Update Person")
+  return appTransaction { People.updateWithRequest(id, request) } > 0
+}
+
+suspend fun PeopleService.createPerson(request: PersonRequest): PersonResponse {
+  application.log.info("Create Person")
+  return appTransaction { Person.newWithRequest(request) }.toResponse()
 }
