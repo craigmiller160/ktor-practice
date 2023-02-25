48bb99783d46148bee2130593cbbd02f9fc83ea4
Craig Miller <craigmiller160@gmail.com>
2023-02-25 13:46:24 -0500

    Fixing db modulFixing db modulee

--- src/main/kotlin/io/craigmiller160/database/Module.kt
+++ src/main/kotlin/io/craigmiller160/database/Module.kt
@@ -1,9 +1,19 @@
 package io.craigmiller160.database
 
-import io.craigmiller160.plugins.initializeDatabase
 import io.ktor.server.application.Application
+import javax.sql.DataSource
+import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.Database
+import org.koin.ktor.ext.inject
 
 fun Application.databaseModule() {
   dependencies()
   initializeDatabase()
+}
+
+fun Application.initializeDatabase() {
+  val datasource by inject<DataSource>()
+  Database.connect(datasource)
+
+  Flyway.configure().dataSource(datasource).load().migrate()
 }
