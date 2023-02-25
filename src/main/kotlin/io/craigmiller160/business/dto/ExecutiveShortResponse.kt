0122bf87f2ddec81b20830e4de227b401f0070db
Craig Miller <craigmiller160@gmail.com>
2023-02-25 16:12:58 -0500

    Tweaked one of the responses

--- src/main/kotlin/io/craigmiller160/business/dto/ExecutiveShortResponse.kt
+++ src/main/kotlin/io/craigmiller160/business/dto/ExecutiveShortResponse.kt
@@ -5,8 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ExecutiveShortResponse(
-    val executiveOfficeId: UUIDAsString,
-    val employeeId: UUIDAsString,
+    val id: UUIDAsString,
     val shortTitle: String,
     val employeeName: String
 )
