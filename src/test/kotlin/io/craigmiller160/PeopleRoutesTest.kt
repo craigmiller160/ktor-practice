8f7ed31f49024be2a981a58e82f5ef24a00d9667
Craig Miller <craigmiller160@gmail.com>
2023-02-25 13:40:44 -0500

    Saving progress

--- src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
@@ -1,15 +1,25 @@
 package io.craigmiller160
 
-import io.craigmiller160.testcontainers.common.TestcontainersExtension
+import io.craigmiller160.testcontainers.common.config.ContainerConfig
+import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
+import io.craigmiller160.testcontainers.common.core.TestcontainerInitializer
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 
-@ExtendWith(TestcontainersExtension::class)
 class PeopleRoutesTest {
+  companion object {
+    init {
+      val result =
+          TestcontainerInitializer.initialize(
+              TestcontainersCommonConfig(
+                  postgres = ContainerConfig(enable = true),
+                  keycloak = ContainerConfig(enable = false)))
+      println("JDBC URL: ${result.postgresContainer?.jdbcUrl}")
+    }
+  }
   @Test
   fun testRoot() = testApplication {
     client.get("/people").apply { assertEquals(HttpStatusCode.OK, status) }
