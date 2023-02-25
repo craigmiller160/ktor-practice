61b2580407c0739389ae41321c6eab0e7c90795e
Craig Miller <craigmiller160@gmail.com>
2023-02-25 10:55:28 -0500

    Progress

--- src/test/kotlin/io/craigmiller160/ApplicationTest.kt
+++ src/test/kotlin/io/craigmiller160/ApplicationTest.kt
@@ -1,11 +1,12 @@
 package io.craigmiller160
 
-import io.craigmiller160.plugins.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
-import kotlin.test.*
+import io.craigmiller160.plugins.configureRouting
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
   @Test
