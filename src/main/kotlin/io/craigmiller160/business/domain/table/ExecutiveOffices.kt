c4bc99c5cf2b3fb9dc954c3df2859954dc01e8c4
Craig Miller <craigmiller160@gmail.com>
2023-02-25 16:16:11 -0500

    Working on tables

--- src/main/kotlin/io/craigmiller160/business/domain/table/ExecutiveOffices.kt
+++ src/main/kotlin/io/craigmiller160/business/domain/table/ExecutiveOffices.kt
@@ -3,6 +3,7 @@ package io.craigmiller160.business.domain.table
 import org.jetbrains.exposed.dao.id.UUIDTable
 
 object ExecutiveOffices : UUIDTable("executive_offices") {
+  val businessId = uuid("business_id").references(Businesses.id)
   val shortTitle = char("short_title", 3)
   val longTitle = char("long_title", 255)
 }
