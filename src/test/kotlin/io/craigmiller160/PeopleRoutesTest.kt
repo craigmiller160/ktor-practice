ea309d15d8e764d538bf0b3f8ffbc607074358f4
Craig Miller <craigmiller160@gmail.com>
2023-02-27 16:19:06 -0500

    Got first test to pass

--- src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
@@ -1,32 +1,17 @@
 package io.craigmiller160
 
-import io.craigmiller160.testcontainers.common.config.ContainerConfig
-import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
-import io.craigmiller160.testcontainers.common.core.TestcontainerInitializer
+import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(TestcontainersExtension::class)
 class PeopleRoutesTest {
-  companion object {
-    init {
-      val result =
-          TestcontainerInitializer.initialize(
-              TestcontainersCommonConfig(
-                  postgres = ContainerConfig(enable = true),
-                  keycloak = ContainerConfig(enable = false)))
-      println("JDBC URL: ${result.postgresContainer?.jdbcUrl}")
-    }
-  }
   @Test
   fun testRoot() = testApplication {
     client.get("/people").apply { assertEquals(HttpStatusCode.OK, status) }
-  }
-
-  @Test
-  fun foo() {
-    println(System.getProperty("testcontainers.common.postgres.url"))
   }
 }
