09d509110f1dba322d6ed013fb3f3ea23872fc56
Craig Miller <craigmiller160@gmail.com>
2023-02-27 17:17:48 -0500

    Got data setup and cleanup working

--- src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/PeopleRoutesTest.kt
@@ -1,13 +1,19 @@
 package io.craigmiller160
 
+import io.craigmiller160.people.domain.entity.Person
+import io.craigmiller160.people.domain.table.People
 import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.TestApplication
 import io.ktor.test.dispatcher.testSuspend
 import kotlin.test.assertEquals
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -29,6 +35,24 @@ class PeopleRoutesTest {
       app.stop()
     }
   }
+
+  @BeforeEach
+  fun setup() {
+    transaction {
+      repeat(3) { index ->
+        Person.new {
+          name = "Person_$index"
+          age = 50 + index
+        }
+      }
+    }
+  }
+
+  @AfterEach
+  fun cleanup() {
+    transaction { People.deleteAll() }
+  }
+
   @Test
   fun testRoot() = testSuspend {
     app.client.get("/people").apply { assertEquals(HttpStatusCode.OK, status) }
